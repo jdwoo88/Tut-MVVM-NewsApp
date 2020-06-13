6a93b278ac62afd30be5e1030fa7e5768b0657b5
John Woo <jdwoo.88@yahoo.com>
2020-06-13 16:05:29 +0800

    Completed Part 7: https://youtu.be/1FGlU6r5o1w - Architectural Skeleton

---
+++ app/src/main/java/com/jwoo/newsapp/respository/NewsRepository.kt
@@ -0,0 +1,6 @@
+package com.jwoo.newsapp.respository
+
+import com.jwoo.newsapp.db.ArticleDatabase
+
+class NewsRepository(val db: ArticleDatabase) {
+}
---
+++ app/src/main/java/com/jwoo/newsapp/ui/NewsViewModel.kt
@@ -0,0 +1,8 @@
+package com.jwoo.newsapp.ui
+
+import androidx.lifecycle.ViewModel
+import com.jwoo.newsapp.respository.NewsRepository
+
+class NewsViewModel(val newsRepository: NewsRepository) : ViewModel() {
+
+}
