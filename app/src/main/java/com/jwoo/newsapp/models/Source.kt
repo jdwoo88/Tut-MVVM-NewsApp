edfbaa19c23c03df1fb7c1977d7c1d13b7a6e9e9
John Woo <jdwoo.88@yahoo.com>
2020-06-13 14:28:40 +0800

    Completed Part 3: https://youtu.be/dr-CFz1j_8k - Setting Retrofit Instance

---
+++ app/src/main/java/com/jwoo/newsapp/models/Source.kt
@@ -0,0 +1,6 @@
+package com.jwoo.newsapp.models
+
+data class Source(
+    val id: Any,
+    val name: String
+)
---
+++ app/src/main/java/com/jwoo/newsapp/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.jwoo.newsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY: String = "1b8d481e50d84b23b1a816dd41ac41d2" // API KEY
+
+        const val BASE_URL: String = "https://newsapi.org/" // Base URL of the API
+    }
+}
